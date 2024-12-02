4305410f235395f37887d9f5e05da21c546912a5
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-12-02 20:50:33 +0100

    refa: move html & css to files

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,16 +2,7 @@ import {  Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  template: `
-    <h2>Navbar</h2>
-    <p>Dummy component</p>
-    <button>Submit</button>
-  `,
-  styles: `
-    h2 {
-      color: red;
-      background: black;
-    }
-  `
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
 })
 export class appNavbar {}
