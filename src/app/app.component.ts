b83bea81da2a5a817b60594b7d4a861ce0085ebf
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-12-06 19:43:53 +0100

    feat: old way ngIf directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 import { AppNavbar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'this string loaded dynamically';
-  imgURL: string = 'https://d585tldpucybw.cloudfront.net/sfimages/default-source/blogs/2023/2023-11/angular-logo-1200-303.png?sfvrsn=d8bbe35c_3';
-  imgAlt: string = 'angular-picture';
-  isDisabled: boolean = false;
-  isActive: boolean = false;
-  fruitsName: string = 'apple';
-  textValue: string = 'This data comes from cmponent';
-
-  userName: string = 'John Doe';
-
-  buttonClick() {
-    alert('clicked button')
-  }
-
-  keyEnter(e: KeyboardEvent) {
-    if (e.key === 'Enter') alert('Enter key pressed')
-  }
-
-  keyupFiltering(user: HTMLInputElement) {
-    console.log(user.id)
-  }
-
-  updateUsername(username: HTMLInputElement) {
-    this.userName = username.value;
-    console.log(this.userName);
-  }
+  isLoggedIn: boolean = true;
+  userName:string = 'John Doe';
 }
